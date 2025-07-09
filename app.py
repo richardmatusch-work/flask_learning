9adb9dc408e195bf10b928efc636f9f872430702
richardmatusch <richard.matusch@swida.sk>
2025-07-09 15:11:03 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "👋 Hello, Flask world!"
+    
+@app.route("/about")
+def about():
+    return "This is a tiny Flask app. 🧪"
+
+if __name__ == "__main__":
+    app.run(debug=True)
